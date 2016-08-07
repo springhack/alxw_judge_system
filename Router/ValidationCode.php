34cb30a036eb540042cf38108afc71cfb9f4aa5e
SpringHack <springhack@live.cn>
2016-08-07 09:51:07 +0800

    Add demo

---
+++ Router/ValidationCode.php
@@ -0,0 +1,23 @@
+<?php /**
+        Author: SpringHack - springhack@live.cn
+        Last modified: 2016-08-04 22:03:17
+        Filename: Index.php
+        Description: Created by SpringHack using vim automatically.
+**/ ?>
+<?php
+
+    //Import class ValidationCode
+    import('Util.ValidationCode');
+
+    Router::uses('/ValidationCode$/', function ($param, $url) {
+
+        
+        //Instance of class ValidationCode
+        $c = new ValidationCode(80, 20, 4);
+        //Show ValidationCode
+        $c->showImage();
+        
+
+    });
+
+?>
